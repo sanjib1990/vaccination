26b8593db3d4ea29de20b8a1b82bbc002aabb2eb
sanjib1990 <devnath.sanjib@gmail.com>
2021-05-26 22:23:31 +0530

    initial Commit

--- flightplan.js
+++ flightplan.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 let plan    = require("flightplan");
 let args    = require('minimist')(process.argv.slice(2));
 let getCodeFolder = () => {
-    return "/var/www/html/web";
+    return "/home/ubuntu/vaccination";
 };
 
 let envs    = {
@@ -25,8 +25,7 @@ plan.target('deploy', {
 //Deploy Plan
 plan.remote(remote => {
     remote.with('cd ' + getCodeFolder(), () => {
-        remote.log("Server maintanence mode on");
-        remote.exec("php artisan down");
+        remote.log("!! STARTING UPDATE !!");
 
         remote.log("Fetching all the branches.");
         remote.exec("git fetch --all --prune");
@@ -37,42 +36,14 @@ plan.remote(remote => {
         remote.log("Pull from git repository.");
         remote.exec("git pull origin "+ envs.branch);
 
-        remote.log("Instaling composer dependencies");
-        remote.exec("composer install");
+        remote.log("Instaling dependencies");
+        remote.exec("/home/ubuntu/.local/bin/pip install -r requirements.txt");
 
-        remote.log("Instaling Node dependencies");
-        remote.exec("~/.nvm/versions/node/v6.11.2/bin/node ~/.nvm/versions/node/v6.11.2/bin/npm install");
-
-        remote.log("Combine assets");
-        remote.exec("~/.nvm/versions/node/v6.11.2/bin/node ~/.nvm/versions/node/v6.11.2/bin/npm run dev");
-
-        remote.log("Running migration");
-        remote.exec("php artisan migrate");
-
-        remote.log("Update Asset Version");
-        remote.exec("php artisan update:cache");
-
-        remote.log("Permission for shared folders");
-        remote.exec("sudo chmod -R 777 bootstrap");
-        remote.exec("sudo chmod -R 777 storage");
-        remote.exec("sudo chmod -R 777 public");
-
-        remote.log("Server maintanence mode off");
-        remote.exec("php artisan up");
+        remote.log("!! UPDATE DONE !!");
     });
 });
 
 plan.local(local => {
-   let Slack    = require("slack-node");
-   let slack    = new Slack(process.env.SLACK_API_TOKEN);
-
-    slack.api('chat.postMessage', {
-        username: "UBU-Deployer",
-        icon_emoji: ":space_invader:",
-        text:'Deploy Successfull on `'+ envs.host +'` Branch: `'+ envs.branch + '` Codebase: `website`',
-        channel:'#deploy'
-    }, function(err, response){
-    });
 });
 
 function getUserHome() {
