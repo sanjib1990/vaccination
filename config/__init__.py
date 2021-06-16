ba8f798bf6277514495c8a939297dd22d709a636
Sanjib <devnath.sanjib@gmail.com>
2021-06-16 09:43:33 +0530

    Clean up ignored files

--- config/__init__.py
+++ config/__init__.py
@@ -10,6 +10,6 @@ path = os.path.abspath(os.path.join(
 load_dotenv(dotenv_path=path)
 
 SLACK_HOOK_URL = os.getenv('SLACK_HOOK_URI')
-AGES = [int(x.strip()) for x in os.getenv('AGES').strip().split(",")]
-PINCODES = [int(x.strip()) for x in os.getenv('PINCODES').strip().split(",")]
+AGES = [int(x.strip()) for x in os.getenv('AGES').strip().split(",") if x.strip()]
+PINCODES = [int(x.strip()) for x in os.getenv('PINCODES').strip().split(",") if x.strip()]
 DAYS = int(os.getenv('DAYS', 1))
